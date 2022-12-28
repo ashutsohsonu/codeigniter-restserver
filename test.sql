52c2a9b00236e392d2259d892705c2ef844a0513
Ashutosh <ashutosh.mca.delhi@gmail.com>
2022-12-28 10:32:33 +0530

    done

---
+++ test.sql
@@ -0,0 +1,64 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.18-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for test_for_php
+CREATE DATABASE IF NOT EXISTS `test_for_php` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `test_for_php`;
+
+-- Dumping structure for table test_for_php.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `amt` int(11) DEFAULT NULL,
+  `item_name` varchar(150) DEFAULT NULL,
+  `scheduled_pickup` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table test_for_php.orders: ~2 rows (approximately)
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` (`id`, `user_id`, `amt`, `item_name`, `scheduled_pickup`) VALUES
+	(1, 1, 78, 'test', '2023-01-01'),
+	(2, 1, 78, 'test', '2023-01-01');
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+
+-- Dumping structure for table test_for_php.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `mail` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table test_for_php.users: ~11 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `mail`) VALUES
+	(1, 'onu', 'sonu@dd.com'),
+	(3, 'ashutosh', 'ashu@gg.com'),
+	(4, 'ashutosh', 'ashu@gg.com'),
+	(5, '', 'ashu@gg.com'),
+	(6, '', 'ashu@gg.com'),
+	(7, '', 'ashu@gg.com'),
+	(8, 'aa', 'ashu@gg.com'),
+	(9, 'aa', 'ashu@gg.com'),
+	(10, 'aa', 'ashu@gg.com'),
+	(11, 'aa', 'ashu@gg.com'),
+	(12, 'aa', 'ashu@gg.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
